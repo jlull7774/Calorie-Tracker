12d82a8b2859382212ea10c4a2eeaae215073d55
Jason Lull <jlull7774@gmail.com>
2022-07-26 16:25:37 -0400

    added is_active field

--- backend/calorie/models.py
+++ backend/calorie/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-
+    is_active = models.BooleanField(default=True)
