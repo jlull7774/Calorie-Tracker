c50a693aae7a25bf85f3974fb38008e30972ab53
Jason Lull <jlull7774@gmail.com>
2022-07-29 13:08:24 -0400

    added provider to app.js and passed in store as component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,24 +8,29 @@ import Activate from "./containers/Activate"
 import ResetPassword from "./containers/ResetPassword"
 import ResetPasswordConfirm from "./containers/ResetPasswordConfirm"
 
+import { Provider } from "react-redux"
+import store from "./store"
+
 import Layout from "./hoc/Layout"
 
 const App = () => (
-	<Router>
-		<Layout>
-			<Routes>
-				<Route path='/' element={<Home/>} />
-				<Route path='/login' element={<Login/>} />
-				<Route path='/signup' element={<Signup/>} />
-				<Route path='/reset-password' element={<ResetPassword/>} />
-				<Route
-					path='/password/reset/confirm/:uid/:token'
-					element={<ResetPasswordConfirm/>}
-				/>
-				<Route path='/activate/:uid/:token' element={<Activate/>} />
-			</Routes>
-		</Layout>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<Signup />} />
+					<Route path='/reset-password' element={<ResetPassword />} />
+					<Route
+						path='/password/reset/confirm/:uid/:token'
+						element={<ResetPasswordConfirm />}
+					/>
+					<Route path='/activate/:uid/:token' element={<Activate />} />
+				</Routes>
+			</Layout>
+		</Router>
+	</Provider>
 )
 
 export default App
