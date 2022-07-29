7cb9908999c7ec6d7467e93671a7a5e05742b715
Jason Lull <jlull7774@gmail.com>
2022-07-29 12:58:23 -0400

    added imports to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,2 +1,5 @@
 import { configureStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
