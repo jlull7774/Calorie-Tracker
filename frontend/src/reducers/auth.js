0b1ffff92282865ee257e675cc8c0236f591a1a7
Jason Lull <jlull7774@gmail.com>
2022-07-29 13:17:07 -0400

    added switch case to auth

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,7 +1,40 @@
 import {
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOAD_USER_SUCCESS,
-    LOAD_USER_FAIL,
-} from '../actions/reducers/types'
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOAD_USER_SUCCESS,
+	LOAD_USER_FAIL,
+} from "../actions/reducers/types"
 
+const initialState = {
+	access: localStorage.getItem("access"),
+	refresh: localStorage.getItem("refresh"),
+	isAuthenticated: null,
+	user: null,
+}
+
+export default function (state = initialState, action) {
+	const { type, payload } = action
+
+	switch (type) {
+		case LOGIN_SUCCESS:
+			localStorage.setItem("access", payload.access)
+			return {
+				...state,
+				isAuthenticated: true,
+				access: payload.access,
+				refresh: payload.refresh,
+			}
+		case LOGIN_FAIL:
+			localStorage.removeItem("access")
+			localStorage.removeItem("refresh")
+			return {
+				...state,
+				access: null,
+				refresh: null,
+				isAuthenticated: false,
+				user: null,
+			}
+		default:
+			return state
+	}
+}
