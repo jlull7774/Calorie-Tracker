0ecd06d2402a1e730a002ba6001dad11dd8c7ddc
Jason Lull <jlull7774@gmail.com>
2022-07-30 20:35:56 -0400

    edited home!

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,10 +1,16 @@
-import React from "react"
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
-	<div>
-		Home
-	</div>
-)
+    <div className='container'>
+        <div class='jumbotron mt-5'>
+            <h1 class='display-4'>Welcome to the Calorie Tracker!</h1>
+            <p class='lead'>Track your calories now!</p>
+            <hr class='my-4' />
+            <p>Click the Log In button</p>
+            <Link class='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
+        </div>
+    </div>
+);
 
-export default Home
+export default Home;
