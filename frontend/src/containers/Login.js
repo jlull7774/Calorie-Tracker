f193e1f70a85ddcaca4b22f6538f85166d89bc3d
Jason Lull <jlull7774@gmail.com>
2022-07-30 20:39:36 -0400

    added reset password functionality

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,70 +1,98 @@
-import React, { useState } from "react"
-import { Link, Redirect } from "react-router-dom"
-import { connect } from "react-redux"
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { login } from '../actions/reducers/auth';
+import axios from 'axios';
 
-const Login = () => {
-	const [formData, setFormData] = useState({
-		email: "",
-		password: "",
-	})
-	const { email, password } = formData
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value })
+const Login = ({ login, isAuthenticated }) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '' 
+    });
 
-	const onSubmit = (e) => {
-		e.preventDefault()
+    const { email, password } = formData;
 
-		// login(email,password)
-	}
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	// Is the user authenticated
-	// Redirect them to the home page
+    const onSubmit = e => {
+        e.preventDefault();
 
-	return (
-		<div className='container mt-5'>
-			<h1>Sign In</h1>
-			<p>Sign into your Account</p>
-			<form onSubmit={(e) => onSubmit(e)}>
-				<div className='form-group mb-3'>
-					<input
-						className='form-control'
-						type='email'
-						placeholder='Email'
-						name='email'
-						value={email}
-						onChange={(e) => onChange(e)}
-						required
-					/>
-				</div>
-				<div className='form-group mb-3'>
-					<input
-						className='form-control'
-						type='password'
-						placeholder='Password'
-						name='password'
-						value={password}
-						onChange={(e) => onChange(e)}
-						minLength='6'
-						required
-					/>
-				</div>
-				<button className='btn btn-primary' type='submit'>
-					Login
-				</button>
-			</form>
-			<p className='mt-3'>
-				Don't have an account? <Link to='/signup'>Sign Up</Link>
-			</p>
-			<p className='mt-3'>
-				Frogot your Password?
-				<Link to='/reset-password'>Reset Password</Link>
-			</p>
-		</div>
-	)
-}
+        login(email, password);
+    };
 
-// const mapStateToProps = state => ({
-// 	is authenticated?
-// })
+    const continueWithGoogle = async () => {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/google-oauth2/?redirect_uri=${process.env.REACT_APP_API_URL}/google`)
 
-export default connect(null, {  })(Login)
+            window.location.replace(res.data.authorization_url);
+        } catch (err) {
+
+        }
+    };
+
+    const continueWithFacebook = async () => {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/facebook/?redirect_uri=${process.env.REACT_APP_API_URL}/facebook`)
+
+            window.location.replace(res.data.authorization_url);
+        } catch (err) {
+
+        }
+    };
+
+    if (isAuthenticated) {
+        return <Navigate to='/' />
+    }
+
+    return (
+        <div className='container mt-5'>
+            <h1>Sign In</h1>
+            <p>Sign into your Account</p>
+            <form onSubmit={e => onSubmit(e)}>
+                <div className='form-group mb-3'>
+                    <input
+                        className='form-control'
+                        type='email'
+                        placeholder='Email'
+                        name='email'
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group mb-3'>
+                    <input
+                        className='form-control'
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength='6'
+                        required
+                    />
+                </div>
+                <button className='btn btn-primary' type='submit'>Login</button>
+            </form>
+            <button className='btn btn-danger mt-3' onClick={continueWithGoogle}>
+                Continue With Google
+            </button>
+            <br />
+            <button className='btn btn-primary mt-3' onClick={continueWithFacebook}>
+                Continue With Facebook
+            </button>
+            <p className='mt-3'>
+                Don't have an account? <Link to='/signup'>Sign Up</Link>
+            </p>
+            <p className='mt-3'>
+                Forgot your Password? <Link to='/reset-password'>Reset Password</Link>
+            </p>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
