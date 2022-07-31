62d340515c54927fa1829339682bb333cb095eff
Jason Lull <jlull7774@gmail.com>
2022-07-30 20:21:28 -0400

    added types and imports

--- frontend/src/componenets/Navbar/NavBar.js
+++ frontend/src/componenets/Navbar/NavBar.js
@@ -1,6 +1,7 @@
-import React from "react"
-
-
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/reducers/auth';
 const NavBar = () => (
 	<div>
 		
